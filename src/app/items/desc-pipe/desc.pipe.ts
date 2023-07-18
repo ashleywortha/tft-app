6d7f87362f88c3191816bcc86bab242b4415e1ab
Ashley Wortham <ashleywortham@Ashleys-MacBook-Pro.local>
2023-07-18 16:04:10 -0400

    minor desc pipe fix

--- src/app/items/desc-pipe/desc.pipe.ts
+++ src/app/items/desc-pipe/desc.pipe.ts
@@ -8,12 +8,6 @@ export class DescPipe implements PipeTransform {
   transform(value: any, effects: any[]): unknown {
     var newStr = value;
 
-    //Grant 45 bonus starting Mana. 
-    //Once per combat: At 40% Health, 
-    //gain a 25% maximum Health shield that lasts 
-    //up to 5 seconds and 35 Armor and 
-    //35 Magic Resist for the rest of combat
-
     for( const e in effects){
       var re = "@" + e + "@"
       newStr = newStr.replace(re, effects[e])
@@ -21,6 +15,7 @@ export class DescPipe implements PipeTransform {
 
     newStr = newStr.replace("<tftitemrules>", "")
     newStr = newStr.replace("</tftitemrules>", "")
+    newStr = newStr.replace(/%.*?%/, "");
 
 
     return newStr;
